7b94046f36d9459b38ae6f82dbd64bd59a32838e
huangyuqin <10097@LAPTOP-I2R8FKV2>
2017-12-15 10:17:58 +0800

    资源概览更新

--- src/main/java/com/govmade/zhdata/module/drs/web/AssetsStatController.java
+++ src/main/java/com/govmade/zhdata/module/drs/web/AssetsStatController.java
@@ -189,26 +189,29 @@ public class AssetsStatController {
     public ResponseEntity<Map<String, Object>> queryCountList(){
         Map<String, Object> map1=Maps.newHashMap();
         
-        Company comp =new Company();
+      /*  Company comp =new Company();
         comp.setDelFlag(Global.DEL_FLAG_NORMAL);
         Integer compCount=0;
-        if (this.companyMapper.selectCount(comp)>0) {
-            compCount=this.companyMapper.selectCount(comp);
-        }
+        Integer cCount=this.companyMapper.selectCount(comp);
+        if (cCount >0) {
+            compCount=cCount;
+        }*/
         
         Element elee=new Element();
         elee.setDelFlag(Global.DEL_FLAG_NORMAL);
         Integer eleCount=0;
-        if (this.elementMapper.selectCount(elee)>0) {
-            eleCount=this.elementMapper.selectCount(elee);
+        Integer eCount=this.elementMapper.selectCount(elee);
+        if (eCount >0) {
+            eleCount=eCount;
         }
         Information infoo=new Information();
         infoo.setDelFlag(Global.DEL_FLAG_NORMAL);
         Integer infoCount=0;
-        if (this.informationMapper.selectCount(infoo)>0) {
-            infoCount=this.informationMapper.selectCount(infoo);
+        Integer iCount=this.informationMapper.selectCount(infoo);
+        if (iCount >0) {
+            infoCount=iCount;
         }
-        map1.put("compCount", compCount);
+        //map1.put("compCount", compCount);
         map1.put("eleCount", eleCount);
         map1.put("infoCount", infoCount);
         
@@ -229,7 +232,7 @@ public class AssetsStatController {
     @RequestMapping(value="ass/queryCountList",method=RequestMethod.GET)
     public ResponseEntity<Page<Map<String, Object>>> list(Page<Company> page,String companyIds) {
         
-       // page = this.companyService.getPageForSearch(page);
+        page = this.companyService.getPageForSearch(page);
         
        /* Map<String, Object> map1=Maps.newHashMap();
         
@@ -262,7 +265,7 @@ public class AssetsStatController {
         company.setDelFlag(Global.DEL_FLAG_NORMAL);*/
         //List<Company> companylList=this.companyMapper.select(company);
         
-        page.startPage();
+        //page.startPage();
         Integer total=this.companyService.queryTotal(page,companyIds);
         List<Company> companylList=this.companyService.queryListById(page,companyIds);
         List<Map<String, Object>> list=Lists.newArrayList();
@@ -274,57 +277,65 @@ public class AssetsStatController {
             sys.setCompanyId(companyId);
             sys.setDelFlag(Global.DEL_FLAG_NORMAL);
             Integer sCount=0;
-            if (this.systemMapper.selectCount(sys)>0) {
-                sCount=this.systemMapper.selectCount(sys);
+            Integer sCount1=this.systemMapper.selectCount(sys);
+            if (sCount1 >0) {
+                sCount=sCount1;
             }
             Element ele=new Element();
             ele.setCompanyId(companyId);
             ele.setDelFlag(Global.DEL_FLAG_NORMAL);
             Integer eCount=0;
-            if (this.elementMapper.selectCount(ele)>0) {
-                eCount=this.elementMapper.selectCount(ele);
+            Integer eCount1=this.elementMapper.selectCount(ele);
+            if (eCount1>0) {
+                eCount=eCount1;
             }
             Information info=new Information();
             info.setCompanyId(companyId);
             info.setDelFlag(Global.DEL_FLAG_NORMAL);
             Integer iCount=0;
-            if (this.informationMapper.selectCount(info)>0) {
-                iCount=this.informationMapper.selectCount(info);
+            Integer inCount=this.informationMapper.selectCount(info);
+            if (inCount >0) {
+                iCount=inCount;
             }
             Information info1=new Information();
             info1.setCompanyId(companyId);
             info1.setInfoType1(1);
             info1.setDelFlag(Global.DEL_FLAG_NORMAL);
             Integer iCount1=0;
-            if (this.informationMapper.selectCount(info1)>0) {
-                iCount1=this.informationMapper.selectCount(info1);
+            Integer infCount1=this.informationMapper.selectCount(info1);
+            if (infCount1>0) {
+                iCount1=infCount1;
             }
             Information info2=new Information();
             info2.setCompanyId(companyId);
             info2.setInfoType1(51);
             info2.setDelFlag(Global.DEL_FLAG_NORMAL);
             Integer iCount2=0;
-            if (this.informationMapper.selectCount(info2)>0) {
-                iCount2=this.informationMapper.selectCount(info2);
+            Integer inforCount2=this.informationMapper.selectCount(info2);
+            if (inforCount2>0) {
+                iCount2=inforCount2;
             }
             
             
             Integer iCount3=0;
             String shareType="1";
-            if (this.informationDao.queryInfoByshareType(companyId,shareType)>0) {
-                iCount3=this.informationDao.queryInfoByshareType(companyId,shareType);
+            Integer isCount1=this.informationDao.queryInfoByshareType(companyId,shareType);
+            if (isCount1>0) {
+                iCount3=isCount1;
             }
             
             Integer iCount4=0;
             shareType="2";
-            if (this.informationDao.queryInfoByshareType(companyId,shareType)>0) {
-                iCount4=this.informationDao.queryInfoByshareType(companyId,shareType);
+            Integer isCount2=this.informationDao.queryInfoByshareType(companyId,shareType);
+            if (isCount2>0) {
+                iCount4=isCount2;
             }
             
             Integer iCount5=0;
             shareType="3";
-            if (this.informationDao.queryInfoByshareType(companyId,shareType)>0) {
-                iCount5=this.informationDao.queryInfoByshareType(companyId,shareType);
+            Integer isCount3=this.informationDao.queryInfoByshareType(companyId,shareType);
+            if (isCount3 >0) {
+                iCount5=isCount3;
             }
             
             map2.put("companyId", c.getId());
