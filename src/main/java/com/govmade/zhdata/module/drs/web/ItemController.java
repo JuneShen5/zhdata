9db6a77f6aed468e35a44f2465618eeb07bd8732
ruoruo <1009729675@qq.com>
2018-03-08 15:49:46 +0800

    数据元控制器更新

--- src/main/java/com/govmade/zhdata/module/drs/web/ItemController.java
+++ src/main/java/com/govmade/zhdata/module/drs/web/ItemController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "catalog/code")
-public class CodeController {
+@RequestMapping(value = "catalog/item")
+public class ItemController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String toCode() {
-        return "modules/catalog/codeIndex";
+    public String toCItem() {
+        return "modules/catalog/itemIndex";
     }
 
     
